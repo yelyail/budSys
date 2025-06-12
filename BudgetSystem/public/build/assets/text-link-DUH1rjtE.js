318d9cf4dd1c96a7cd109a7b1f71b985c3d5ad21
Ariel <114245987+yelyail@users.noreply.github.com>
2025-06-12 21:08:27 +0800

    initial

---
+++ BudgetSystem/public/build/assets/text-link-DUH1rjtE.js
@@ -0,0 +1,1 @@
+import{j as t,$ as n}from"./app-llYc-8xj.js";import{a}from"./app-logo-icon-BbGvJYL7.js";function u({className:r="",children:e,...o}){return t.jsx(n,{className:a("text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500",r),...o,children:e})}export{u as T};
